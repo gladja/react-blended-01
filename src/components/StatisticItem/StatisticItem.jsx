6a58aa70ccfc1f96291b1eda918bd06c8c4a217b
gladja <gladkih.alexander@gmail.com>
2023-09-08 20:18:20 +0300

    first commit

---
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -0,0 +1,9 @@
+import {
+  StatisticBox,
+  StatisticText,
+  StatisticCounter,
+} from './StatisticItem.styled';
+
+export const StatisticItem = ({ title }) => {
+  return <div>{title && <h2>Hello</h2>} </div>;
+};
